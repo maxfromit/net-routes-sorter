ad896672d5714d9400ca67bedfb14e9ecbac5f96
maximfromit <bigmaxcapacity@gmail.com>
2025-06-10 20:09:16 +0300

    IndexPage: update title to display active IPv4 routes in Russian

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import NetRoutesTable from 'src/components/NetRoutesTable/NetRoutesTable.vue';
+import NetRoutesTable from 'src/components/NetRoutesTable/NetRoutesTable.vue'
 </script>
 
 <template>
   <q-page class="column">
     <div class="col col-grow column">
-      <div class="text-h5 q-pt-md q-px-sm q-pb-sm">The Routes</div>
+      <div class="text-h5 q-pt-md q-px-sm q-pb-sm">Действующие маршруты IPv4</div>
       <NetRoutesTable class="col" />
     </div>
   </q-page>
