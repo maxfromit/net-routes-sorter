7180c75262d9c65462d610818c35a1c3a5a7b3e3
maximfromit <bigmaxcapacity@gmail.com>
2025-06-10 20:07:43 +0300

    NetRoutesTable columns.ts: add sorting functions and function to calculating address by ip + maks

--- src/components/NetRoutesTable/columns.ts
+++ src/components/NetRoutesTable/columns.ts
@@ -1,19 +1,30 @@
-import type { QTableProps } from 'quasar';
+import type { QTableProps } from 'quasar'
+import type { Route } from 'src/components/NetRoutesTable/NetRoutesTable.vue'
+
+import { createNetmaskBlock } from 'src/components/NetRoutesTable/utils/createNetmaskBlock'
+import {
+  compareIp,
+  compareRoutesByNetwork,
+} from 'src/components/NetRoutesTable/utils/compareFunctions'
 
 export const columns = <QTableProps['columns']>[
   {
     name: 'address',
     label: 'Адрес назначения',
-    field: 'address',
+    field: (row: Route) => row,
+    format: (val: Route) => createNetmaskBlock(val.address, val.mask).toString(),
     sortable: true,
     align: 'left',
+    sort: compareRoutesByNetwork,
   },
+
   {
     name: 'gateway',
     label: 'Шлюз',
     field: 'gateway',
     sortable: true,
     align: 'left',
+    sort: compareIp,
   },
   {
     name: 'interface',
@@ -22,4 +33,4 @@ export const columns = <QTableProps['columns']>[
     sortable: true,
     align: 'left',
   },
-];
+]
